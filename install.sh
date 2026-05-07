c1d51a810ab482ec7fcfd9fe8c8e752345d07e7b
KJ Roberts <kjroberts39@hotmail.com>
2026-05-07 12:36:58 -0600

    Remove auto-restart; keep install script free of sudo

--- install.sh
+++ install.sh
@@ -23,11 +23,5 @@ curl -sSfL "$ZIP_URL" -o "$TMP/${PLUGIN_NAME}.zip"
 rm -rf "${PLUGINS_DIR}/${PLUGIN_NAME}"
 unzip -q "$TMP/${PLUGIN_NAME}.zip" -d "$PLUGINS_DIR"
 
-if systemctl is-active --quiet plugin_loader.service 2>/dev/null; then
-  echo "Restarting Decky Loader..."
-  sudo systemctl restart plugin_loader.service
-  echo "Done! ${PLUGIN_NAME} should appear shortly."
-else
-  echo ""
-  echo "Done! Restart Decky Loader (or reboot) to load ${PLUGIN_NAME}."
-fi
+echo ""
+echo "Done! Restart Decky Loader (or reboot) to load ${PLUGIN_NAME}."
